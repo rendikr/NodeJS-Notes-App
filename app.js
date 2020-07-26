5de18cfe30d18a13f10e30af4230e9a293c9e875
Rendi K <rendi.krinaldi@gmail.com>
2020-07-26 22:18:15 +0700

    importing npm package module

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const validator = require('validator')
 const getNotes = require('./notes')
 
 console.log(getNotes())
+
+console.log(validator.isEmail('gmail.com'))
+console.log(validator.isURL('https://mead.io'))
