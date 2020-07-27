86d6a57cbeb5e48f2b0d4aa7e44383e634805354
Rendi K <rendi.krinaldi@gmail.com>
2020-07-27 22:46:11 +0700

    Argument parsing with Yargs NPM Package: Part 2

--- app.js
+++ app.js
@@ -9,8 +9,21 @@ yargs.version('1.1.0')
 yargs.command({
   command: 'add',
   describe: 'Add a new note',
-  handler: function () {
-    console.log('Adding a new note')
+  builder: {
+    title: {
+      describe: 'The note title',
+      demandOption: true, // this parameter will set the "required" option
+      type: 'string'
+    },
+    body: {
+      describe: 'The note contents',
+      demandOption: true, // this parameter will set the "required" option
+      type: 'string'
+    }
+  },
+  handler: function (argv) {
+    console.log('Title: ', argv.title)
+    console.log('Body: ', argv.body)
   }
 })
 
